3ffdd098f9f797e9c11f922afb80f693de15f24c
MisterRuby <ruby8700@naver.com>
2023-01-24 22:02:19 +0900

    init : 프로젝트 생성

---
+++ src/main/java/ruby/sql/SqlApplication.java
@@ -0,0 +1,15 @@
+package ruby.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlApplication.class, args);
+    }
+
+}
